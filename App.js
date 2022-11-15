e491a5b9bb677ec09f6f42bb056b4576bdae290b
Prajna1999 <gituprajna20@gmail.com>
2022-11-16 00:35:51 +0530

    add splash screen

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import LoginScreen, {SocialButton} from "react-native-login-screen";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  Button,
+  TouchableOpacity,
+} from "react-native";
 export default function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+ 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <LoginScreen
+// logoImageSource={require("./assets/logo.png")}
+  onLoginPress={() => {}}
+  onSignupPress={() => {}}
+  onEmailChange={(email) => {setEmail}}
+  onPasswordChange={(password) => {setPassword}}
+  textInputContainerStyle={{color:'red'}}
+  disableSocialButtons
+/>
+{/* <SocialButton text="Continue with Google"  /> */}
       <StatusBar style="auto" />
+      
     </View>
   );
+
+    
+  
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
+ 
+  loginscreen:{
+    backgroundColor:'red',
+  }
+  
 });
